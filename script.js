5d413185eb80f3f114ee8beea085b9a2c2b2c18d
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-08-22 16:12:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Get references to the browser-info div
+        var browserInfoDiv = document.getElementById("browser-info");
+
+        // Get browser information from the navigator object
+        var userAgent = navigator.userAgent;
+        var browserName = navigator.appName;
+        var browserVersion = navigator.appVersion;
+
+        // Display the browser information in the div
+        browserInfoDiv.textContent = "You are using " + browserName + " version " + browserVersion;
+
+        // You can also use template literals for a cleaner syntax
+        // browserInfoDiv.textContent = `You are using ${browserName} version ${browserVersion}`;
